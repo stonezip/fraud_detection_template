45b80f3ffb23978faa196c4237296d2217524b24
shiwuzhang <shiwuzhang@insightzen.com>
2019-03-08 13:58:39 +0800

    +11

--- SkFraudDetection.py
+++ SkFraudDetection.py
@@ -20,7 +20,7 @@ class SkFraudDetection(object):
         feature_names : array of feature names (optional)
         """
         print("Predict called - will run idenity function")
-        return X+10
+        return X+11
 
     def send_feedback(self,features,feature_names,reward,truth):
         """
